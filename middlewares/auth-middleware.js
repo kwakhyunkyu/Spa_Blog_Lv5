114e7b9724a7896540332a36efa987706734e708
kwakhyunkyu <paul971222@gmail.com>
2023-06-23 18:06:48 +0900

    Spa_Blog_Lv3

---
+++ middlewares/auth-middleware.js
@@ -0,0 +1,38 @@
+const jwt = require("jsonwebtoken");
+const { Users, Tokens } = require("../models");
+
+module.exports = async (req, res, next) => {
+  const { authorization } = req.cookies;
+  const [tokenType, token] = authorization.split(" ");
+  if (tokenType !== "Bearer" || !token) {
+    return res.status(401).json({
+      message: "토큰 타입이 존재하지 않거나 토큰이 일치하지 않습니다.",
+    });
+  }
+
+  try {
+    const decodedToken = jwt.verify(token, "customize_secret_key");
+    const userId = decodedToken.userId;
+
+    const user = await Users.findOne({
+      where: { userId },
+    });
+
+    if (!user) {
+      return res.status(401).json({
+        message: "토큰에 해당하는 사용자가 존재하지 않습니다.",
+      });
+    }
+
+    res.locals.user = {
+      userId: user.userId,
+      nickname: user.nickname,
+    };
+
+    next();
+  } catch (error) {
+    res.status(401).json({
+      message: "비정상적인 접근입니다.",
+    });
+  }
+};
