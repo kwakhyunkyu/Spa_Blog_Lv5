beb4155e2c8d82acf3571207a30e8e0b5071dfc0
kwak hyun kyu <paul971222@gmail.com>
2023-07-14 12:33:36 +0900

    Lv5 초기작업. 아직 손도 못댐.

---
+++ routes/comments.route.js
@@ -0,0 +1,116 @@
+const express = require('express');
+const { Posts, Comments } = require('../models');
+const authMiddleware = require('../middlewares/auth-middleware');
+const router = express.Router();
+
+// 댓글 생성
+router.post('/posts/:postId/comments', authMiddleware, async (req, res) => {
+  const { userId, nickname } = res.locals.user;
+  const { postId } = req.params;
+  const { comment } = req.body;
+
+  try {
+    // 게시글이 존재하는지 확인
+    const post = await Posts.findOne({ where: { postId } });
+    if (!post) {
+      return res.status(404).json({ message: '게시글이 존재하지 않습니다.' });
+    }
+
+    // 댓글 생성
+    const createdComment = await Comments.create({
+      UserId: userId,
+      PostId: postId,
+      Nickname: nickname,
+      comment,
+    });
+
+    return res.status(201).json({ data: createdComment });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: '서버 오류입니다.' });
+  }
+});
+
+// 댓글 목록 조회
+router.get('/posts/:postId/comments', async (req, res) => {
+  const { postId } = req.params;
+
+  try {
+    // 게시글이 존재하는지 확인
+    const post = await Posts.findOne({ where: { postId } });
+    if (!post) {
+      return res.status(404).json({ message: '게시글이 존재하지 않습니다.' });
+    }
+
+    // 댓글 목록 조회
+    const comments = await Comments.findAll({
+      where: { PostId: postId },
+      order: [['createdAt', 'DESC']],
+    });
+
+    return res.status(200).json({ data: comments });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: '서버 오류입니다.' });
+  }
+});
+
+// 댓글 수정 API
+router.put('/posts/:postId/comments/:commentId', authMiddleware, async (req, res) => {
+  const { userId } = res.locals.user;
+  const { postId, commentId } = req.params;
+  const { comment } = req.body;
+
+  if (!comment) {
+    return res.status(400).json({ message: '댓글 내용을 입력해주세요.' });
+  }
+
+  try {
+    const editComment = await Comments.findOne({
+      where: {
+        commentId,
+        PostId: postId,
+        UserId: userId,
+      },
+    });
+
+    if (!editComment) {
+      return res.status(404).json({ message: '댓글이 존재하지 않습니다.' });
+    }
+
+    editComment.comment = comment;
+    await editComment.save();
+
+    return res.status(200).json({ data: editComment });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: '서버 오류입니다.' });
+  }
+});
+
+// 댓글 삭제 API
+router.delete('/posts/:postId/comments/:commentId', authMiddleware, async (req, res) => {
+  const { userId } = res.locals.user;
+  const { postId, commentId } = req.params;
+
+  try {
+    const deleteComment = await Comments.findOne({
+      where: {
+        commentId,
+        PostId: postId,
+        UserId: userId,
+      },
+    });
+
+    if (!deleteComment) {
+      return res.status(404).json({ message: '댓글이 존재하지 않습니다.' });
+    }
+
+    await deleteComment.destroy();
+
+    return res.status(200).json({ message: '댓글이 삭제되었습니다.' });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: '서버 오류입니다.' });
+  }
+});
--- routes/posts.route.js
+++ routes/posts.route.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { Op } = require('sequelize');
-const { Posts, Comments, Likes } = require('../models');
+const { Posts, Likes } = require('../models');
 const authMiddleware = require('../middlewares/auth-middleware');
 const router = express.Router();
 
@@ -53,7 +53,7 @@ router.put('/posts/:postId', authMiddleware, async (req, res) => {
   if (!post) {
     return res.status(404).json({ message: '게시글이 존재하지 않습니다.' });
   } else if (post.UserId !== userId) {
-    return res.status(401).json({ message: '권한이 없습니다.' });
+    return res.status(403).json({ message: '권한이 없습니다.' });
   }
 
   // 게시글의 권한을 확인하고, 게시글을 수정합니다.
@@ -80,7 +80,7 @@ router.delete('/posts/:postId', authMiddleware, async (req, res) => {
   if (!post) {
     return res.status(404).json({ message: '게시글이 존재하지 않습니다.' });
   } else if (post.UserId !== userId) {
-    return res.status(401).json({ message: '권한이 없습니다.' });
+    return res.status(403).json({ message: '권한이 없습니다.' });
   }
 
   // 게시글의 권한을 확인하고, 게시글을 삭제합니다.
@@ -91,118 +91,6 @@ router.delete('/posts/:postId', authMiddleware, async (req, res) => {
   });
 
   return res.status(200).json({ data: '게시글이 삭제되었습니다.' });
-});
-
-// 댓글 생성
-router.post('/posts/:postId/comments', authMiddleware, async (req, res) => {
-  const { userId, nickname } = res.locals.user;
-  const { postId } = req.params;
-  const { comment } = req.body;
-
-  try {
-    // 게시글이 존재하는지 확인
-    const post = await Posts.findOne({ where: { postId } });
-    if (!post) {
-      return res.status(404).json({ message: '게시글이 존재하지 않습니다.' });
-    }
-
-    // 댓글 생성
-    const createdComment = await Comments.create({
-      UserId: userId,
-      PostId: postId,
-      Nickname: nickname,
-      comment,
-    });
-
-    return res.status(201).json({ data: createdComment });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ message: '서버 오류입니다.' });
-  }
-});
-
-// 댓글 목록 조회
-router.get('/posts/:postId/comments', async (req, res) => {
-  const { postId } = req.params;
-
-  try {
-    // 게시글이 존재하는지 확인
-    const post = await Posts.findOne({ where: { postId } });
-    if (!post) {
-      return res.status(404).json({ message: '게시글이 존재하지 않습니다.' });
-    }
-
-    // 댓글 목록 조회
-    const comments = await Comments.findAll({
-      where: { PostId: postId },
-      order: [['createdAt', 'DESC']],
-    });
-
-    return res.status(200).json({ data: comments });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ message: '서버 오류입니다.' });
-  }
-});
-
-// 댓글 수정 API
-router.put('/posts/:postId/comments/:commentId', authMiddleware, async (req, res) => {
-  const { userId } = res.locals.user;
-  const { postId, commentId } = req.params;
-  const { comment } = req.body;
-
-  if (!comment) {
-    return res.status(400).json({ message: '댓글 내용을 입력해주세요.' });
-  }
-
-  try {
-    const editComment = await Comments.findOne({
-      where: {
-        commentId,
-        PostId: postId,
-        UserId: userId,
-      },
-    });
-
-    if (!editComment) {
-      return res.status(404).json({ message: '댓글이 존재하지 않습니다.' });
-    }
-
-    editComment.comment = comment;
-    await editComment.save();
-
-    return res.status(200).json({ data: editComment });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ message: '서버 오류입니다.' });
-  }
-});
-
-// 댓글 삭제 API
-router.delete('/posts/:postId/comments/:commentId', authMiddleware, async (req, res) => {
-  const { userId } = res.locals.user;
-  const { postId, commentId } = req.params;
-
-  try {
-    const deleteComment = await Comments.findOne({
-      where: {
-        commentId,
-        PostId: postId,
-        UserId: userId,
-      },
-    });
-
-    if (!deleteComment) {
-      return res.status(404).json({ message: '댓글이 존재하지 않습니다.' });
-    }
-
-    await deleteComment.destroy();
-
-    return res.status(200).json({ message: '댓글이 삭제되었습니다.' });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ message: '서버 오류입니다.' });
-  }
 });
 
 // 좋아요 생성 또는 취소 API
