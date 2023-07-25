7c7fd3472775b5b19a116f8a1b38edfe1707c38d
kwakhyunkyu <paul971222@gmail.com>
2023-07-25 15:31:13 +0900

    Lv4 수정, .env 수정

--- middlewares/auth-middleware.js
+++ middlewares/auth-middleware.js
@@ -1,17 +1,17 @@
-const jwt = require("jsonwebtoken");
-const { Users } = require("../models");
+const jwt = require('jsonwebtoken');
+const { Users } = require('../models');
 
 module.exports = async (req, res, next) => {
   const { authorization } = req.cookies;
-  const [tokenType, token] = authorization.split(" ");
-  if (tokenType !== "Bearer" || !token) {
+  const [tokenType, token] = authorization.split(' ');
+  if (tokenType !== 'Bearer' || !token) {
     return res.status(401).json({
-      message: "토큰 타입이 존재하지 않거나 토큰이 일치하지 않습니다.",
+      message: '토큰 타입이 존재하지 않거나 토큰이 일치하지 않습니다.',
     });
   }
 
   try {
-    const decodedToken = jwt.verify(token, "customize_secret_key");
+    const decodedToken = jwt.verify(token, 'customize_secret_key');
     const userId = decodedToken.userId;
 
     const user = await Users.findOne({
@@ -19,8 +19,8 @@ module.exports = async (req, res, next) => {
     });
 
     if (!user) {
-      return res.status(401).json({
-        message: "토큰에 해당하는 사용자가 존재하지 않습니다.",
+      return res.status(404).json({
+        message: '토큰에 해당하는 사용자가 존재하지 않습니다.',
       });
     }
 
@@ -32,7 +32,7 @@ module.exports = async (req, res, next) => {
     next();
   } catch (error) {
     res.status(401).json({
-      message: "비정상적인 접근입니다.",
+      message: '비정상적인 접근입니다.',
     });
   }
 };
---
+++ routes/likes.routes.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const { Likes } = require('../models');
+const authMiddleware = require('../middlewares/auth-middleware');
+const router = express.Router();
+
+// 좋아요 생성 또는 취소 API
+router.post('/posts/:postId/likes', authMiddleware, async (req, res) => {
+  const { userId } = res.locals.user;
+  const { postId } = req.params;
+
+  try {
+    const existingLike = await Likes.findOne({
+      where: {
+        PostId: postId,
+        UserId: userId,
+      },
+    });
+
+    if (existingLike) {
+      // 이미 좋아요를 누른 경우 좋아요 취소
+      await Likes.destroy({
+        where: {
+          PostId: postId,
+          UserId: userId,
+        },
+      });
+
+      return res.status(200).json({ message: '게시글의 좋아요를 취소했습니다.' });
+    }
+
+    // 좋아요 생성
+    await Likes.create({
+      PostId: postId,
+      UserId: userId,
+    });
+
+    return res.status(201).json({ message: '게시글에 좋아요를 눌렀습니다.' });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: '서버 오류입니다.' });
+  }
+});
+
+module.exports = router;
